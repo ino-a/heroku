e08f7bf6b158a310aa125da7ee4cb9cd08dc7665
inoue <inosho2000@ezweb.ne.jp>
2022-02-09 03:19:33 +0900

    first commit

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ secret.")
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
+    if event.reply_token == "00000000000000000000000000000000":
+       return
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
