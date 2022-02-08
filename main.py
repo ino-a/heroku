42a586f6149e1363d7864bbacfbe8d119ec4e39d
inoue <inosho2000@ezweb.ne.jp>
2022-02-09 01:54:38 +0900

    first commit

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def callback():
     try:
         handler.handle(body,signature)
     except InvalidSignatureError:
-        print("Invalid signature.Please check your channel access token/channel"
-              "secret")
+        print("Invalid signature.Please check your channel access token/channel
+secret.")
         abort(400)
     return 'OK'
 
