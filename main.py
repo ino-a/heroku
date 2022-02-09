cc98711e1d66619a2a99a68f4982f6a0d5556d03
inoue <inosho2000@ezweb.ne.jp>
2022-02-09 20:36:16 +0900

    first commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask, request, abort
 import os
 
 from linebot import (
-    LineBotApi,WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent,TextMessage,TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
@@ -19,20 +19,19 @@ LINE_CHANNEL_SECRERT = os.environ["LINE_CHANNEL_SECRERT"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRERT)
 
-@app.route("/callback",methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     app.logger.info("Request body: "+body)
     try:
-        handler.handle(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature.Please check your channel access token/channel
-secret.")
+        print("Invalid signature.Please check your channel access token/channel secret.")
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent,message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
     line_bot_api.reply_message(
@@ -41,5 +40,5 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
