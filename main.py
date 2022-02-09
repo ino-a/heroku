84f2fef0e9f7478376c6a4a022dbd8b3b9cd2724
inoue <inosho2000@ezweb.ne.jp>
2022-02-09 21:39:58 +0900

    first commit

--- main.py
+++ main.py
@@ -1,65 +1,44 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, abort
+import os
 
-from janome.tokenizer import Tokenizer
-
-import requests
-import json
-import re
-
-import settings
-
-LINEBOT_API_EVENT ='https://trialbot-api.line.me/v1/events'
-LINE_HEADERS = {
-    'Content-type': 'application/json; charset=UTF-8',
-    'X-Line-ChannelID':settings.CHANNEL_ID,
-    'X-Line-ChannelSecret':settings.CHANNEL_SECRET,
-    'X-Line-Trusted-User-With-ACL':settings.MID
-}
-
-def post_event(to, content):
-    msg = {
-        'to': [to],
-        'toChannel': 1383378250,
-        'eventType': "138311608800106203",
-        'content': content
-    }
-    r = requests.post(LINEBOT_API_EVENT, headers = LINE_HEADERS, data = json.dumps(msg))
-
-def post_text(to, text):
-    content = {
-        'contentType':1,
-        'toType':1,
-        'text':text,
-    }
-    post_event(to, content)
-
-
-commands = (
-    (re.compile('作者', 0), lambda x: 'https://nnsnodnb.moe'),
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    messages = request.json['result']
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: "+body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature.Please check your channel access token/channel secret.")
+        abort(400)
+    return 'OK'
 
-    for message in messages:
-        text = message['content']['text']
-        for matcher, action in commands:
-            if matcher.search(text):
-                response = action(text)
-                break
-        else:
-            post_text(message['content']['from'], '解析中...')
-            # 形態素解析
-            response = ''
-            t = Tokenizer()
-            for token in t.tokenize(message['content']['text']):
-                response += str(token) + '\n'
-        post_text(message['content']['from'], response)
-    return ''
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 8001, threaded = True, debug = True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
